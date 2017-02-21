8dd13b7ba6312654c1395ee636975ff780d9b7a3
abraham <abr314@icloud.com>
2017-02-21 00:23:54 -0600

    commit

--- src/networking/ab_grand_central_dispatch.rs
+++ src/networking/ab_grand_central_dispatch.rs
@@ -1,29 +1,25 @@
-
 pub mod ab_grand_central_dispatch {
 
     pub struct ABGrandCentralDispatch {
-        open_dispatch_queues:Vec<ABDispatchQueue>,
+        manager_queue:Vec<ABOrderManager>,
     }
 
     impl ABGrandCentralDispatch {
         pub fn new()->ABGrandCentralDispatch {
             ABGrandCentralDispatch {
-                open_dispatch_queues:Vec::new(),
+                manager_queue:Vec::new(),
             }
         }
-        fn push_queue_to_gcd(mut self, queue:ABDispatchQueue) {
-            self.open_dispatch_queues.push(queue)
-        }
 
-        fn pop_queue_from_gcd(mut self) -> ABDispatchQueue{
-            self.open_dispatch_queues.pop().unwrap() as ABDispatchQueue
-        }
-
-        fn receive_message(message:ABGCDMessage) {
+        pub fn register_manager(manager:&ABOrderManager) {
 
         }
 
-        fn send_message_to_owned_queue(message:ABGCDMessage) {
+        pub fn receive_message(message:ABGCDMessage) {
+
+        }
+
+        fn send_message(message:ABGCDMessage) {
 
         }
 
@@ -37,7 +33,91 @@ pub mod ab_grand_central_dispatch {
 
     }
 
-    struct ABDispatchQueue {
+    pub struct ABOrderManager {
+        // crea
+        orders:Vec<ABOrder>,
+        count:i32,
+    }
+
+    impl ABOrderManager {
+
+        pub fn new() -> ABOrderManager {
+            ABOrderManager {
+                count:0,
+                orders:Vec::new()
+            }
+        }
+
+        fn send_message(message:ABGCDMessage) {
+            ABGrandCentralDispatch::receive_message(message);
+        }
+        pub fn receive_message(message:ABGCDMessage) {
+
+
+        }
+
+
+        fn process_tick_message(message:ABGCDMessage) {
+
+        }
+
+        fn update_order(&mut self) {
+
+        }
+        fn create_new_order(&mut self) {
+            // push
+        }
+        fn close_order(&mut self) {
+            // remove specific
+        }
+        fn close_all_orders(&mut self) {
+            //
+        }
+    }
+
+    struct ABOrder {
+        product_id:i32,
+        security_type:SecurityType,
+        order_id:i32,
+        order_type:OrderType,
+        order_side:OrderSide,
+        order_status:OrderStatus,
+        // adjust according to market conditions
+        stop_value:Option<f32>,
+        limit_value:Option<f32>
+
+    }
+
+    enum SecurityType {
+        Future,
+        Equity,
+        Option,
+        Bond,
+        Forex,
+    }
+
+    enum OrderStatus {
+        Created,
+        Pending,
+        Open,
+        Filled,
+        Closed,
+        Cancelled
+    }
+
+    enum OrderType {
+        Market,
+        Limit
+    }
+
+    enum OrderSide {
+        Long,
+        Short
+    }
+    struct ABProduct {
+        symbol:String,
+        full_name:String,
+        product_type:String,
 
     }
     pub struct ABGCDMessage {
@@ -45,6 +125,8 @@ pub mod ab_grand_central_dispatch {
         message:String,
         value:i32,
     }
+
+
 
 
 
